9589672a140faf77d24a290f3d10d891d1d9faca
loc <steven.ngtloc@gmail.com>
2018-04-13 07:44:21 +0000

    add login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
    end
    
    def create
-   
+      chef = Chef.find_by(email: params[:session][:email].downcase)
+      if chef && chef.authenticate(params[:session][:password])
+         session[:chef_id] = chef.id
+         flash[:success] = "Login successfully"
+         redirect_to chef
+      else
+         flash.now[:danger] = "There was something wrong with your login infomation"
+         render 'new'
+      end
    end
    
    def destroy
-       
+       session[:chef_id] = nil
+       flash[:success] = "Logout successfully"
+       redirect_to root_path
    end
 end
